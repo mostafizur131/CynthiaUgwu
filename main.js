f64c63fdd2e2603ad3e67c52f7e7209c5c7a99d5
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2023-08-19 22:42:45 +0600

    animation added to hero section

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+function firstPageAnimation() {
+  let tl = gsap.timeline();
+  tl.from("#nav", {
+    y: -10,
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingElem", {
+      y: 0,
+      opacity: 1,
+      duration: 1.5,
+      ease: Expo.easeInOut,
+      delay: -1,
+      stagger: 0.2,
+    })
+    .from("#heroFooter", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      ease: Expo.easeInOut,
+      delay: -1,
+    });
+}
+
 function circleMouseFollower() {
   window.addEventListener("mousemove", (dets) => {
     let circle = document.querySelector("#mouseCircle");
@@ -6,3 +31,4 @@ function circleMouseFollower() {
 }
 
 circleMouseFollower();
+firstPageAnimation();
